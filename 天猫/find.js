205965333296a1c66a6fd37e555a1e3affbb5bce
liukexina <2578871731@qq.com>
2018-07-02 13:56:00 +0800

    软件工程实验5

---
+++ 天猫/find.js
@@ -0,0 +1,5 @@
+var search = document.getElementsByClassName("search")[0];
+var behave = document.getElementsByClassName("behave")[0];
+search.onclick = function () {
+    behave.style.color ="rgb(204, 204, 204)";
+};
