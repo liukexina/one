a1d9b507da76a036299974190dd99fd93e6141e0
liukexina <2578871731@qq.com>
2018-08-09 16:31:46 +0800

    8-9-（2）

--- vuecli/vuedome/src/components/ConnectCom1.vue
+++ vuecli/vuedome/src/components/ConnectCom1.vue
@@ -1,7 +1,33 @@
 <template>
     <div>
-        <h1>子路由1</h1>
-        <h2>姓名：{{$route.params.name}}</h2>
-        <h2>年龄：{{$route.params.age}}</h2>
+        <nav class="navbar navbar-light bg-light justify-content-between" style="border-radius:5%;width: 50%; margin:0 auto">
+            <a class="navbar-brand">Navbar</a>
+            <form class="form-inline">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
+        </nav>
+        <div class="card" style="width: 18rem; margin:0 auto">
+            <img class="card-img-top" src="../assets/tu.jpg" alt="Card image cap" style="margin:0 auto;transform: scale(0.8);height: 180px;width:180px;">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+        <div class="box">
+            <h1>子路由1</h1>
+            <h2>姓名：{{$route.params.name}}</h2>
+            <h2>年龄：{{$route.params.age}}</h2>
+        </div>
     </div>
 </template>
+
+<style scoped>
+    .box{
+        width: 1142px;
+        height: 150px;
+        background: #f3f3f3;
+        margin: 0 auto;
+    }
+</style>
--- vuecli/vuedome/src/views/Connect.vue
+++ vuecli/vuedome/src/views/Connect.vue
@@ -1,10 +1,14 @@
 <template>
     <div>
-        <h1>联系我们</h1>
+        <h1>联系我们{{$store.state.count}}</h1>
         <router-link :to="{name:'test1',params:{name:'liuke',age:30}}">test1</router-link> |
         <router-link to="/connect/test2/zhangsan/20">test2</router-link> |
         <router-link to="/connect/backtest2/zhangsan/20">back test2</router-link>
-        <router-view/>
+        <div class="router-tran">
+            <transition name="fad">
+                <router-view/>
+            </transition>
+        </div>
     </div>
 </template>
 
@@ -15,5 +19,31 @@
 </script>
 
 <style scoped>
-
+    .router-tran{
+        position: relative;
+    }
+    .fad-enter{
+        transform: scale(0.5);
+    }
+    .fad-enter-active{
+        transition: all 1s ease;
+    }
+    .fad-enter-to{
+        position:absolute;
+        top:0;
+        width: 100%;
+        transform: scale(1);
+    }
+    .fad-leave{
+        transform: scale(1);
+    }
+    .fad-leave-active{
+        transition: all 0.5s ease;
+    }
+    .fad-leave-to{
+        position:absolute;
+        top:0;
+        width: 100%;
+        transform: scale(0.5);
+    }
 </style>
