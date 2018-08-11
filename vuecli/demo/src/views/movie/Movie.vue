680240676024068de735a9fa50318274d4dd6e24
liukexina <2578871731@qq.com>
2018-08-11 14:54:30 +0800

    8-11-（4）

--- vuecli/demo/src/views/movie/Movie.vue
+++ vuecli/demo/src/views/movie/Movie.vue
@@ -1,26 +1,28 @@
 <template>
     <div>
         <div class="container">
-            {{isend}}
             <ul class="data">
                 <li v-for="(items,index) in dataList" :key='index+ "nl"'>
-                    <img v-bind:src="items.images.large" alt="">
-                    <span class="info">
+                    <img v-bind:src="items.images.large" alt="" class="tu">
+                    <div class="info">
                         <span>{{items.title}}</span><br>
                         导演: <span v-for="(item) in items.directors" >{{item.name}}/</span><br>
                         <span v-for="(item,index) in items.casts" :key="index+'ul'">{{item.name}}/</span><br>
                         <span>票房:{{items.collect_count}}</span><br>
                         类型:<span v-for="(item) in items.genres">{{item}}/</span><br>
                         <a v-bind:href="items.alt">详情</a>
-                    </span>
+                    </div>
                 </li>
             </ul>
+        </div>
+        <div class="image" v-show="load">
+            <img src="@/assets/image/loading.gif" alt="">
         </div>
     </div>
 </template>
 
 <style scoped>
-    img{
+    .tu{
         margin:10px;
         display: block;
         width: 120px;
@@ -35,6 +37,17 @@
     .data li{
         overflow: hidden;
     }
+    .image {
+        position: fixed;
+        padding: 1rem;
+        text-align: center;
+        background: rgba(255, 255, 255, 0.5);
+        border-radius: 0.2rem;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        z-index:100;
+    }
 </style>
 <script>
     export default {
@@ -44,7 +57,9 @@
                 classname:"movie-nav",
                 arr:[true,false,false,false],
                 dataList:[],
-                isend:false
+                isend:false,
+                load:true,
+                isshow:false
             }
         },
         created(){
@@ -53,16 +68,20 @@
         },
         methods:{
             getData(){
-                axios.get(API_PROXY+'https://api.douban.com/v2/movie/in_theaters?start'+this.dataList.length+'&count=10')
+                axios.get(API_PROXY+'https://api.myjson.com/bins/nsb9g')
+                    //https://api.douban.com/v2/movie/in_theaters?start='+this.dataList.length+'&count=10'
+                    //https://api.myjson.com/bins/nsb9g
                     .then( (response) => {
-                        this.dataList = response.data.subjects;
-                        if(this.datalist.length == response.data.subject.length){
+                        // console.log(response);
+                        this.dataList = this.dataList.concat(response.data.subjects);
+                        this.load = false;
+                        if(response.data.subjects.length == 0){
                             this.isend = true;
                         }
-                        // console.log(response);
+                        console.log(this.dataList);
                     })
                     .catch((error) => {
-                        console.log(error)
+                        console.log(error);
                     })
             }
         },
@@ -71,8 +90,13 @@
                 let scrollTop = document.documentElement.scrollTop;
                 let scrollHeigth = document.documentElement.scrollHeight;
                 let clientHeight = document.documentElement.clientHeight;
-                if(scrollTop+clientHeight==scrollHeigth){
-                    this.isend = true;
+                if(scrollHeigth - clientHeight - 10 < scrollTop){
+                    if(!this.load){
+                        if(!this.isend){
+                            this.load = true;
+                            this.getData();
+                        }
+                    }
                 }
             }
         }
