d18de378c920eb8f7fb4be115ec90a0fc793a7f6
liukexina <2578871731@qq.com>
2018-08-13 13:53:50 +0800

    8-13-（2）

--- vuecli/demo/src/views/movie/MovieDetail.vue
+++ vuecli/demo/src/views/movie/MovieDetail.vue
@@ -1,12 +1,19 @@
 <template>
     <div class="container">
         <div class="title">{{infoList.title}}</div>
-        <span>{{infoList.ratings_count}}人评价</span>
-        <span>评分：{{infoList.rating.average}}</span>
+        <span v-for="(items,index) in infoList.genres" class="sty">{{items}}</span><br>
+        <div style="text-align: left">
+            <span v-for="(items,index) in infoList.countries">{{items}}/</span>
+            <span>{{infoList.year}}</span>年上映
+            <span style="float:right;color:green;">评分：{{infoList.rating.average}}</span>
+        </div>
         <img :src="infoList.images.large" alt="" class="photo">
         <h5>简介：{{infoList.summary}}</h5>
+        导演：<span v-for="(items,index) in infoList.directors">{{items.name}}/</span><br>
+        主演：<span v-for="(items,index) in infoList.casts" :key="index+'inl'" >{{items.name}}/</span><br>
+        <span style="color: green">{{infoList.ratings_count}}人评价</span>
 
-        <!--主演：<span v-for="(items,index) in infoList.casts" :key="index+'inl'" >{{items.name}}/</span>-->
+
 
     </div>
 </template>
@@ -19,13 +26,29 @@
     h3{
         float:left;
     }
+    h5{
+        margin:0.1rem;
+    }
     .container{
         margin:0.3rem;
+        text-align: left;
     }
     .photo{
         height: 4rem;
         float:right;
         margin:0.1rem;
+        margin-top:0.2rem;
+    }
+    .sty{
+        display: block;
+        height: 0.5rem;
+        width: 1rem;
+        background: #98ffcd;
+        text-align: center;
+        line-height: 0.5rem;
+        border-radius: 0.3rem;
+        float:left;
+        margin-right:0.1rem;
     }
 </style>
 <script>
